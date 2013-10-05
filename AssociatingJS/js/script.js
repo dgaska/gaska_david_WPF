359c9abd9506e9e0f20aa92be27724582e42e8b8
David Gaska <dgaska@fullsail.edu>
2013-10-05 13:40:29 -0500

    Types Numbers Boolean commit

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -13,3 +13,13 @@ age = 30; // definition
 //storage container = value
 
 var yearOfBirth = 1983; //decalring and defining on the same line
+
+// Variables of Different Types, Numbers, Booleans and Strings
+
+var age = 30; // Number Variable
+
+var isStudent = false; //Boolean Variable
+
+var firstName = "David";
+
+var phrase = 'I don\'t know \n You\'ll have to speak with the professor';
