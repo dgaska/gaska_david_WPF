b458345e81e08efce15a015ecce11821f99bb934
David Gaska <dgaska@fullsail.edu>
2013-10-05 13:35:58 -0500

    defining variables

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -6,3 +6,10 @@ console.log("hello World");
 console.log(yearBorn);
 
 alert("Hello World");
+// Defining Variables
+
+var age; //declaration
+age = 30; // definition
+//storage container = value
+
+var yearOfBirth = 1983; //decalring and defining on the same line
