f25ca2d75cc77687eb9075e32ccb68188b65a535
David Gaska <dgaska@fullsail.edu>
2013-10-07 10:01:05 -0500

    checking that I get the right answer.

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,11 @@
+// David Gaska, 10-7-2013 Expressions Worksheet
+
+
+// Dog Years
+
+var sparkysAge = 5; // Sparky's given age in human years
+var dogAge = 7; // a dog's age is 7 times that of its age in human years.
+
+var sparkysDogAge = sparkysAge * dogAge; // The formula to figure sparky's age in dog years.
+
+console.log(sparkysDogAge);
