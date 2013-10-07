1c15f471e02d78162dd50dd1f370ab32588a5812
David Gaska <dgaska@fullsail.edu>
2013-10-07 10:22:51 -0500

    checked my result for slices per person

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -8,6 +8,16 @@ const dogAge = 7; // a dog's age is 7 times that of its age in human years.
 
 var sparkysDogAge = sparkysAge * dogAge; // The formula to figure sparky's age in dog years.
  
-var sparkysResult = "Sparky is" + " " + sparkysAge + " " + "human years which is" + " " + sparkysDogAge + " " + "in dog years."
+var sparkysResult = "Sparky is" + " " + sparkysAge + " " + "human years which is" + " " + sparkysDogAge + " " + "in dog years." // the expression to get the result
 
-console.log(sparkysResult);
+console.log(sparkysResult); // printed result using console.log
+
+// Slice of Pie part 1
+
+var slices = 7.8; // the number of slices per pizza
+const people = 18; // the number of people at the party
+var pizzas = 6; // number of pizzas ordered
+
+var slicesPerPerson = pizzas * slices / people;
+
+console.log(slicesPerPerson);
