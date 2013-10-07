c7e08161d4e709f4f46bcbbfc3ce3828ed80aa24
David Gaska <dgaska@fullsail.edu>
2013-10-07 10:11:47 -0500

    Finished dog age problem

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -7,7 +7,7 @@ var sparkysAge = 5; // Sparky's given age in human years
 const dogAge = 7; // a dog's age is 7 times that of its age in human years.
 
 var sparkysDogAge = sparkysAge * dogAge; // The formula to figure sparky's age in dog years.
-
-var sparkysResult = "Sparky is" + sparkysAge + "human years which is" + sparkysDogAge + "in dog years."
+ 
+var sparkysResult = "Sparky is" + " " + sparkysAge + " " + "human years which is" + " " + sparkysDogAge + " " + "in dog years."
 
 console.log(sparkysResult);
