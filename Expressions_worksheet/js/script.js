2e6075d25aee22d2b94dc5f566e0870f14e626b2
David Gaska <dgaska@fullsail.edu>
2013-10-07 10:02:29 -0500

    change dogAge to const

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -4,7 +4,7 @@
 // Dog Years
 
 var sparkysAge = 5; // Sparky's given age in human years
-var dogAge = 7; // a dog's age is 7 times that of its age in human years.
+const dogAge = 7; // a dog's age is 7 times that of its age in human years.
 
 var sparkysDogAge = sparkysAge * dogAge; // The formula to figure sparky's age in dog years.
 
