098142e75dfd8e5fbe6f926b237c781d0bb9a134
David Gaska <dgaska@fullsail.edu>
2013-10-09 10:44:03 -0500

    added array of choices for formula and equation for oz per day.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,5 +1,11 @@
 // David Gaska 10-9-2013 Expressions Assignment Personal
 
-// How many oz. of formula do you feed your baby in a month
+// How much money do you spend on formula a year.
 
-var timesDay = prompt("I am calcultating how many oz. of formula you feed your baby. \nEnter in how many times a day you feed your baby.")
+var timesDay = prompt("I am calcultating how many oz. of formula you feed your baby. \nEnter in how many times a day you feed your baby."); // This prompt will enter in how many times a day someone feeds their baby.
+
+var ozPerFeeding = prompt("Now enter in how many oz. per feeding your baby gets."); // Prompt to collect how many oz. per feeding they feed their baby!
+
+var ozPerDay = timesDay * ozPerFeeding;
+
+var formulaPrice = ["Enfamil", "Similac", "Gerber", "Parent's Choice", "Up & Up"]; // an array giving a choice to how which formula the buy.
