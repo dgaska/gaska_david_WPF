d2f214055915eb3250c3010044cfd8e63c04325c
David Gaska <dgaska@fullsail.edu>
2013-10-09 12:23:58 -0500

    add ham and turkey prompts and const and variables

---
+++ Expressions_Industry/js/script.js
@@ -0,0 +1,13 @@
+// David Gaska 10-09-2013 Expressions Assignment Industry
+
+// How Many Pounds Of Deli Meat Did You Slice Today?
+
+var ham = prompt("I want to calculate how many pounds of deli meat were sliced today. \nGiven that each ham weighed 5 pounds, How many hams did you slice?");
+
+const hamWeight = 5;
+
+var hamPounds = hamWeight * ham;
+
+var turkey = prompt("Given that the turkeys are 4lbs each. \nHow many turkeys did you slice?");
+
+const turkeyWeight = 4;
