a11fe974ad35110912f900b47ade0d6b21f5b514
David Gaska <dgaska@fullsail.edu>
2013-10-09 12:34:05 -0500

    checked that turkey and chicken prompts and calculations worked properly

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -8,8 +8,21 @@ const hamWeight = 5;
 
 var hamPounds = hamWeight * ham;
 
-console.log(hamPounds);
+//console.log(hamPounds);
 
-//var turkey = prompt("Given that the turkeys are 4lbs each. \nHow many turkeys did you slice?");
+var turkey = prompt("Given that the turkeys are 4lbs each. \nHow many turkeys did you slice?");
 
-//const turkeyWeight = 4;
+const turkeyWeight = 4;
+
+var turkeyPounds = turkeyWeight * turkey;
+
+//console.log(turkeyPounds);
+
+var chicken = prompt("Given that each chicken weighs 3 pounds, \n how many chickens did you slice?");
+
+const chickenWeight = 3;
+
+var chickenPounds = chickenWeight * chicken;
+
+console.log(chickenPounds);
+
