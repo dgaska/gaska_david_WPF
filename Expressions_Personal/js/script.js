019ad6e0c57a3973ea31235d23705d6524572686
David Gaska <dgaska@fullsail.edu>
2013-10-09 10:15:41 -0500

    forgot prompt attribute before added it now

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,4 +2,4 @@
 
 // How many oz. of formula do you feed your baby in a month
 
-var timesDay = ("I am calcultating how many oz. of formula you feed your baby. Enter in how many times a day you feed your baby.")
+var timesDay = prompt("I am calcultating how many oz. of formula you feed your baby. Enter in how many times a day you feed your baby.")
