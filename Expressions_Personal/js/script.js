329a5140ed71d4af62472c4e526aa9196c3c2082
David Gaska <dgaska@fullsail.edu>
2013-10-09 10:17:08 -0500

    entered in a line break to separate the statements

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,4 +2,4 @@
 
 // How many oz. of formula do you feed your baby in a month
 
-var timesDay = prompt("I am calcultating how many oz. of formula you feed your baby. Enter in how many times a day you feed your baby.")
+var timesDay = prompt("I am calcultating how many oz. of formula you feed your baby. \nEnter in how many times a day you feed your baby.")
