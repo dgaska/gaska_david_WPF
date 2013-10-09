60f01d851646915ab99fa504e3544e490112f7b9
David Gaska <dgaska@fullsail.edu>
2013-10-09 10:15:08 -0500

    Entered in first variable and changed title on index.html

---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,5 @@
+// David Gaska 10-9-2013 Expressions Assignment Personal
+
+// How many oz. of formula do you feed your baby in a month
+
+var timesDay = ("I am calcultating how many oz. of formula you feed your baby. Enter in how many times a day you feed your baby.")
