c74f5f806655a6608d351126db56d09189ba6029
David Gaska <dgaska@fullsail.edu>
2013-10-09 10:50:02 -0500

    Changed title to how much you spend per year on formula, and the prompt statement

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,14 +2,15 @@
 
 // How much money do you spend on formula a year.
 
-var timesDay = prompt("I am calcultating how many oz. of formula you feed your baby. \nEnter in how many times a day you feed your baby."); // This prompt will enter in how many times a day someone feeds their baby.
+var timesDay = prompt("I am calcultating how much money you spend on baby formula a year. \nEnter in how many times a day you feed your baby."); // This prompt will enter in how many times a day someone feeds their baby.
 
 var ozPerFeeding = prompt("Now enter in how many oz. per feeding your baby gets."); // Prompt to collect how many oz. per feeding they feed their baby!
 
-var ozPerDay = timesDay * ozPerFeeding;
+var ozPerDay = timesDay * ozPerFeeding; // equation to find how many oz per day they feed their baby.
 
-const days = 365;
+const days = 365; // Days in a year
 
-var ozPerYear = ozPerDay * 365;
+var ozPerYear = ozPerDay * 365; // equation for how many oz in a year they feed their baby.
 
 var formulaPrice = ["Enfamil", "Similac", "Gerber", "Parent's Choice", "Up & Up"]; // an array giving a choice to how which formula the buy.
+
