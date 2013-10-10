1754ed41ef9ef32618db93afd3f9d738f3002bd0
David Gaska <dgaska@fullsail.edu>
2013-10-09 22:49:34 -0500

    added first prompt

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,3 +1,6 @@
 //David Gaska 10-09-2013 Expressions Assignment Wacky
 
-//How many pieces of candy did your kids collect per hour trick or treating
+//How many pieces of candy did your family collect per hour trick or treating
+
+var kids = prompt("I want to calculate how many pieces of candy a family collect per minute trick or treating. \nHow many people in your family trick or treated?");
+
