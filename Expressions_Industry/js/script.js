c06a5ea0b3d21e7c9b3858db938db75989e5c336
David Gaska <dgaska@fullsail.edu>
2013-10-09 20:25:27 -0500

    finished with adding comments to each code

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,32 +2,35 @@
 
 // How Many Pounds Of Deli Meat Did You Slice Today?
 
-var ham = prompt("I want to calculate how many pounds of deli meat were sliced today. \nGiven that each ham weighed 5 pounds, How many hams did you slice?"); 
+var ham = prompt("I want to calculate how many pounds of deli meat were sliced today. \nGiven that each ham weighed 5 pounds, How many hams did you slice?"); //prompt explaining what we are doing and asking how many hams they sliced.
 
-const hamWeight = 5;
+const hamWeight = 5; // how much each ham weighs
 
-var hamPounds = hamWeight * ham;
+var hamPounds = hamWeight * ham; // the calculation of how many pounds of ham were sliced.
 
-//console.log(hamPounds);
+//console.log(hamPounds); // printed result of pounds of ham
 
-var turkey = prompt("Given that the turkeys are 4lbs each. \nHow many turkeys did you slice?");
+var turkey = prompt("Given that the turkeys are 4lbs each. \nHow many turkeys did you slice?"); // prompt asking for number of turkeys sliced
 
-const turkeyWeight = 4;
+const turkeyWeight = 4; // weight of each turkey
 
-var turkeyPounds = turkeyWeight * turkey;
+var turkeyPounds = turkeyWeight * turkey; // calculation of turkey pounds sliced
 
-//console.log(turkeyPounds);
+//console.log(turkeyPounds); //printed result of turkey pounds
 
-var chicken = prompt("Given that each chicken weighs 3 pounds, \n how many chickens did you slice?");
+var chicken = prompt("Given that each chicken weighs 3 pounds, \n how many chickens did you slice?"); //prompt asking how many hams were sliced
 
-const chickenWeight = 3;
+const chickenWeight = 3; // weight of each ham 
 
-var chickenPounds = chickenWeight * chicken;
+var chickenPounds = chickenWeight * chicken; // calculation of pounds of ham sliced
 
-//console.log(chickenPounds);
+//console.log(chickenPounds); // printed result of ham sliced
 
-var totalPounds = hamPounds + turkeyPounds + chickenPounds;
+var totalPounds = hamPounds + turkeyPounds + chickenPounds; // calculation of the total weight of hams
 
 console.log("The total pounds of ham sliced is" + hamPounds +"lbs. The total weight of turkey sliced is" + turkeyPounds +"lbs, and the total weight of chicken sliced is" + chickenPounds + "lbs. Giving you a total of" + totalPounds + "lbs of meat sliced today.");
+// the printed result in console for the whole problem.
+
+
 
 
