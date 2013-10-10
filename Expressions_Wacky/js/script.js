44e0b03ee981ce30ba58153a4423329049c41d50
David Gaska <dgaska@fullsail.edu>
2013-10-09 22:55:37 -0500

    added var total amount of candy family collected

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,5 +2,9 @@
 
 //How many pieces of candy did your family collect per hour trick or treating
 
-var kids = prompt("I want to calculate how many pieces of candy a family collect per minute trick or treating. \nHow many people in your family trick or treated?");
+var members = prompt("I want to calculate how many pieces of candy a family collect per minute trick or treating. \nHow many people in your family trick or treated?");
+
+var candy = prompt("How many pieces of candy did your family collect?");
+
+
 
