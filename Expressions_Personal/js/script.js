f3cfc43de094b215123518df2287ce0e3c52a734
David Gaska <dgaska@fullsail.edu>
2013-10-09 21:22:14 -0500

    changed price to price per oz and got a better result.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,28 +4,28 @@
 
 var timesDay = prompt("I am calcultating how much money you spend on baby formula a year. \nEnter in how many times a day you feed your baby."); // This prompt will enter in how many times a day someone feeds their baby.
 
-var ozPerFeeding = prompt("Now enter in how many oz. per feeding your baby gets."); // Prompt to collect how many oz. per feeding they feed their baby!
+var ozPerFeeding = prompt("Now enter in how many oz. per feeding your baby gets."); // Prompt to collect how many .oz per feeding they feed their baby!
 
-var ozPerDay = timesDay * ozPerFeeding; // equation to find how many oz per day they feed their baby.
+var ozPerDay = timesDay * ozPerFeeding; // equation to find how many .oz per day they feed their baby.
 
 //console.log(ozPerDay);
 
 const days = 365; // Days in a year
 
-var ozPerYear = ozPerDay * 365; // equation for how many oz in a year they feed their baby.
+var ozPerYear = ozPerDay * 365; // equation for how many .oz in a year they feed their baby.
 
 //console.log(ozPerYear);
 
-var formulaPrices = [25, 24, 24, 20, 24];//Array of formula choices 0-Enfamil 1-Similac 2-Gerber 3-Parent's Choice 4-Up & Up.
+var formulaPrices = [.15, .14, .14, .07, .12];//Array of formula choices and their prices per .oz 0-Enfamil 1-Similac 2-Gerber 3-Parent's Choice 4-Up & Up.
 //<<<<<<< HEAD
 
 var formulaChoice = formulaPrices[prompt("Please select the brand you buy. \nEnter in a 0 for Enfamil, 1 for Similac, 2 for Gerber, 3 for Parent's Choice, 4 for Up & Up.")];// a prompt giving a choice to how which formula the buy.
 
-formulaPrices[formulaChoice] = 25;
-formulaPrices[formulaChoice] = 24;
-formulaPrices[formulaChoice] = 24;
-formulaPrices[formulaChoice] = 20;
-formulaPrices[formulaChoice] = 24; 
+formulaPrices[formulaChoice] = .15;
+formulaPrices[formulaChoice] = .14;
+formulaPrices[formulaChoice] = .14;
+formulaPrices[formulaChoice] = .07;
+formulaPrices[formulaChoice] = .12; 
 
 var totalSpent = ozPerYear * formulaChoice;
 
