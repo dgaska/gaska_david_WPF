0f25663b91a94bbff265ceb741b727eb8e3d6391
David Gaska <dgaska@fullsail.edu>
2013-10-14 18:07:39 -0500

    setup folders

---
+++ Conditionals_worksheet/js/script.js
@@ -0,0 +1,10 @@
+// David Gaska 10-2013 Conditionals Worksheet
+
+// Last Chance for Gas!
+
+// Enough Gas?
+// Can they make it without stopping for gas?
+var miles = 200;
+// if less than 200 have to stop for gas.
+
+var 
