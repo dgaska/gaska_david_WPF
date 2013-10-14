f3b14ecc699e7bf403fbb0966a8121b075c88a47
David Gaska <dgaska@fullsail.edu>
2013-10-14 18:32:00 -0500

    added expression for how many miles left in your gas tank

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -7,7 +7,9 @@
 var miles = 200;
 // if less than 200 have to stop for gas.
 
-var mpg = prompt("How many miles per gallon does your car get?");
-var gauge = prompt("What percentage of your gas tank is left?");
-var capacity = prompt("How many gallons of gas does your tank hold?");
+var mpg = prompt("How many miles per gallon does your car get?"); // The miles you can go on a gallon of gas
+var gauge = prompt("What percentage of your gas tank is left?"); // How full your current tank of gas is
+var capacity = prompt("How many gallons of gas does your tank hold?"); // How much your gas tank can hold in gallons
+
+var howFarTotal = mpg * gauge; // How far you can go on your current level of gas
 
