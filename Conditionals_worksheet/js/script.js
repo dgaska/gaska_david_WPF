62c454f7cda45c0e3daad7a7a43c23bf48573761
David Gaska <dgaska@fullsail.edu>
2013-10-14 18:38:35 -0500

    added to gauge prompt to make sure it is changed to decimal

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -8,8 +8,9 @@ var miles = 200;
 // if less than 200 have to stop for gas.
 
 var mpg = prompt("How many miles per gallon does your car get?"); // The miles you can go on a gallon of gas
-var gauge = prompt("What percentage of your gas tank is left?"); // How full your current tank of gas is
+var gauge = prompt("What percentage of your gas tank is left? \nPlease convert to decimal!"); // How full your current tank of gas is
 var capacity = prompt("How many gallons of gas does your tank hold?"); // How much your gas tank can hold in gallons
 
-var howFarTotal = mpg * gauge; // How far you can go on your current level of gas
+var howFarTotal = mpg * capacity * gauge; // How far you can go on your current level of gas
 
+var leftInTank = capacity / gauge
