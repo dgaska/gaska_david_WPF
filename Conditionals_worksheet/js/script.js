9404857d7f2dcf527bddcba3232940c358e454c9
David Gaska <dgaska@fullsail.edu>
2013-10-14 18:42:16 -0500

    changed expressions to make sure the decimal was used for %

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -8,9 +8,12 @@ var miles = 200;
 // if less than 200 have to stop for gas.
 
 var mpg = prompt("How many miles per gallon does your car get?"); // The miles you can go on a gallon of gas
-var gauge = prompt("What percentage of your gas tank is left? \nPlease convert to decimal!"); // How full your current tank of gas is
+var gauge = prompt("What percentage of your gas tank is left?"); // How full your current tank of gas is
 var capacity = prompt("How many gallons of gas does your tank hold?"); // How much your gas tank can hold in gallons
 
-var howFarTotal = mpg * capacity * gauge; // How far you can go on your current level of gas
+var gaugeDecimal = gauge / 100; // changes % to a decimal
 
-var leftInTank = capacity / gauge
+var howFarTotal = mpg * capacity * gaugeDecimal; // How far you can go on your current level of gas
+
+var leftInTank = capacity / gaugeDecimal; // Gives result of how much is left in tank
+
