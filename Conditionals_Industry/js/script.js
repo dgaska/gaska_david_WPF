cb04286fa05ca18c41bc444a16f8c51c95fbea50
David Gaska <dgaska@fullsail.edu>
2013-10-15 13:30:17 -0500

    added expression to figure out packages in cooler also changed problem question

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -4,7 +4,7 @@
 // We sell a lot of packages of chicken leg quarters
 // We usually keep two palllets in the cooler
 // each pallet has 50 cases with 10 packages of chicken in each case
-// How many cases should be order to keep up with weekend sales?
+// Do I need to order for the weekend or do we have enough?
 
 const neededPackages = 1000;
 
@@ -13,6 +13,12 @@ const packagesPerCase = 10;
 var onFloor = prompt("How many packages of chicken are left on the floor?");
 
 var inCooler = prompt("How many cases of chicken are left in the the coooler?");
+
+var coolerPackages = inCooler * packagesPerCase;
+
+
+
+
 
 
 
