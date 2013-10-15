e576f3719464a9c165aee01ca96b384e9775e516
David Gaska <dgaska@fullsail.edu>
2013-10-15 13:50:29 -0500

    running into a problem with result tested coolerPackages variable it worked

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -8,11 +8,10 @@
 // Do I need to order for the weekend or do we have enough?
 // We could also have way too much already and need to drop the price.
 
-const neededPackages = 1000;
+
 
 const packagesPerCase = 10;
 
-const tooMany = 1500
 
 var onFloor = prompt("How many packages of chicken are left on the floor?");
 
@@ -20,15 +19,17 @@ var inCooler = prompt("How many cases of chicken are left in the the coooler?");
 
 var coolerPackages = inCooler * packagesPerCase;
 
-var totalPackages = coolerPackages + onFloor;
+console.log(coolerPackages);
 
-if(totalPackages <= neededPackages){
+/*var totalPackages = coolerPackages + onFloor;*/
+
+/*if(totalPackages <= 1000){
 	console.log("You need to order more chicken leg quarters for the weekend!");
-}else if(totalPackages >= tooMany){
+}else if(totalPackages >= 1500){
 	console.log("You have too many packages already, must reduce the price for the weekend!");
 }else{
 	console.log("You have a good amount, no need to order more.");
-}
+}*/
 
 
 
