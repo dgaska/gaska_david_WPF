ac53c611879e32b8815a96dce2c19e70d16429be
David Gaska <dgaska@fullsail.edu>
2013-10-15 13:45:42 -0500

    was getting the right result added a new variable and changed else if

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -20,10 +20,11 @@ var inCooler = prompt("How many cases of chicken are left in the the coooler?");
 
 var coolerPackages = inCooler * packagesPerCase;
 
+var totalPackages = coolerPackages + onFloor;
 
-if(coolerPackages + onFloor <= neededPackages){
+if(totalPackages <= neededPackages){
 	console.log("You need to order more chicken leg quarters for the weekend!");
-}else if(coolerPackages + onFloor > tooMany){
+}else if(totalPackages >= tooMany){
 	console.log("You have too many packages already, must reduce the price for the weekend!");
 }else{
 	console.log("You have a good amount, no need to order more.");
