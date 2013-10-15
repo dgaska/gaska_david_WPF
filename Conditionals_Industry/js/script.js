b70bf4039f792b25a0453cf520f680d875c2c3d9
David Gaska <dgaska@fullsail.edu>
2013-10-15 13:41:48 -0500

    bad punctuation at end of sentences

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -21,9 +21,9 @@ var inCooler = prompt("How many cases of chicken are left in the the coooler?");
 var coolerPackages = inCooler * packagesPerCase;
 
 if(coolerPackages + inCooler <= neededPackages){
-	console.log("You need to order more chicken leg quarters for the weekend");
+	console.log("You need to order more chicken leg quarters for the weekend!");
 }else if(coolerPackages + inCooler > tooMany){
-	console.log("You have too many packages already, must reduce the price for the weekend1");
+	console.log("You have too many packages already, must reduce the price for the weekend!");
 }else{
 	console.log("You have a good amount, no need to order more.");
 }
