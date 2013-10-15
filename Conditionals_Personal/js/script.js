51872cbf86d8345848c01c90ca254e4b32f40975
David Gaska <dgaska@fullsail.edu>
2013-10-15 12:39:03 -0500

    set up variables and const

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -4,8 +4,17 @@
 // I have been trying to reduce how much soda I drink
 // I am only allowing to myself to have 48oz a day which is a lot less than I normally drink.
 // I mainly drink Mountain Dew
-// Normally I drink cans(12oz) and the bottles(20oz). I also have the 24oz bottles too.
+// Normally I drink cans(12oz) and the bottles(20oz). I also have the big bottles (24oz) too.
 // It is hard with the different sizes it comes in to keep track.
+
+const dailyLimit = 48
+
+var cans = prompt("How many cans(12oz) did you have today?");
+
+var bottles = prompt("How many bottles(20oz) did you drink today?");
+
+var bigBottles = prompt("How many big bottles(24oz) did you have?");
+
 
 
 
