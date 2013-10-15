34dd6a5f5f32388418b31463eb01b2d2546474a9
David Gaska <dgaska@fullsail.edu>
2013-10-15 12:12:51 -0500

    Realized that to solve I would have to use something not learned yet. So changing problem and will save this problem for next week!

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,11 +1,6 @@
 // David Gaska 10-2013 Consditionals Assignment Personl
 
-// Going to Wisconsin Dells
-// We want to go to Chula Vista Resort in the Dells this weekend.
-// Do not know if wife is getting paid until check is posted online due to the government shutdown.
-// Chula Vista offers an all inclusive package.
-// For four adults, two kids ages 2-10, one child aged 11-20 and an infant the cost will be $1500
-// Reservations already made.
-// No paycheck means cancelling the reservation!
+
+
 
 
