7265e1ff6cc879049fca6633cd06ae39de25af3c
David Gaska <dgaska@fullsail.edu>
2013-10-15 12:36:15 -0500

    set up new problem

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,6 +1,11 @@
 // David Gaska 10-2013 Consditionals Assignment Personl
 
-
+// How many cans of soda can I have
+// I have been trying to reduce how much soda I drink
+// I am only allowing to myself to have 48oz a day which is a lot less than I normally drink.
+// I mainly drink Mountain Dew
+// Normally I drink cans(12oz) and the bottles(20oz). I also have the 24oz bottles too.
+// It is hard with the different sizes it comes in to keep track.
 
 
 
