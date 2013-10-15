9212cf2814c3a75dc8116ef301e60922881397ca
David Gaska <dgaska@fullsail.edu>
2013-10-15 13:14:03 -0500

    set up problem

---
+++ Conditionals_Industry/js/script.js
@@ -0,0 +1,10 @@
+// David Gaska 10-2013 Conditional Assignment Industry
+
+// keeping the same hours or reducing hours
+// At my job the hours available depend on weekly sales.
+// In the meat dept we have 1 manager 1 asst manager 2 full time and 4 part time employees
+// The manager and asst manager get 40 hours
+// full time also gets 40 hours
+// the part time employees get 8 hours a week
+// based on sales and salary can we keep everybody's hours  the same or does somebody get reduced?
+
