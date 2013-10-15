78830f21594ce181705ffafc6c45cd7847719e46
David Gaska <dgaska@fullsail.edu>
2013-10-14 22:20:22 -0500

    got part 3 to work

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -17,4 +17,46 @@ var howFarTotal = mpg * capacity * gaugeDecimal; // How far you can go on your c
 
 var leftInTank = capacity * gaugeDecimal; // Gives result of how much is left in tank
 
-console.log(leftInTank);
+//console.log(leftInTank); tested the math and had to fix had wrong symbol in it.
+
+if(howFarTotal > miles){
+	console.log("Yes, you can make it without stopping for gas!");
+}else{
+	console.log("You only have " + leftInTank + " gallons of gas in your tank, better get gas while you can!"); 
+}
+
+// Check the Login
+
+// Right Username
+// What happens when the wrong username and or password are entered in?
+/*var username = prompt("Enter in Username:");
+var password = prompt("Enter in Password");
+
+var correctUsername = "Full Sail";
+var correctPassword = "JavaScript";
+
+
+if(username = correctUsername && password = correctPassword){
+	console.log("Welcome " + correctUsername + "!");
+}else if(username = false){
+	console.log("Username not found. Try again.");
+}else{
+	console.log("Password does not match our records.");
+}*/
+
+// Movie Ticket Price
+// How much will it cost?
+// age of customer and time a factore
+
+var age = prompt("Please enter your age.");
+var time = prompt("please enter the time of movie. Use military time.");
+var price;
+
+if(age < 10 || age >= 55){
+	price = 7.00;
+}else if(time > 15 && time < 17){
+	price = 7.00;
+}else{
+	price = 12.00;
+}
+  console.log("Your ticekt price is $" + price);
