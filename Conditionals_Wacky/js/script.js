d783949f9f8f567b2c648d5723e116a1359ebf7d
David Gaska <dgaska@fullsail.edu>
2013-10-15 22:21:54 -0500

    changed a few things

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -3,8 +3,8 @@
 // Equal acceleration
 // Do my cars have equal accleration?
 
-var car1 = prompt("How long does it take your first car to go 100 yards in seconds?");
+var car1 = prompt("How long does it take your first car to go 100 yards in seconds?"); //How fast car1 goes
 
-var car2 = prompt("How long does it take your second car to go 100 yard in seconds?");
+var car2 = prompt("How long does it take your second car to go 100 yard in seconds?"); //How fast car2 goes
 
 (car1 === car2) ? console.log("Your cars are equal speed") : console.log("One car is faster than the other?");
