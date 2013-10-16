3fbbb8750bae356809ca9abca2196a1526ebce11
David Gaska <dgaska@fullsail.edu>
2013-10-15 22:18:58 -0500

    changed title and changed problem to print out.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,26 +1,10 @@
 // David Gaska 10-2013 Conditionals Assignment Wacky
 
-// Do I have time to do wash
-// I am leaving for vacation tomorrow
-// Do I have time today to do all my laundry
-// How many loads do I have?
-// How much time do I have left?
-// How long does each load take?
-// Will it be done before I leave
+// Equal acceleration
+// Do my cars have equal accleration?
 
-var loads = prompt("How many loads do you have to do?");
+var car1 = prompt("How long does it take your first car to go 100 yards in seconds?");
 
-var washTime = prompt("How long do you put your laundry in the washer for in minutes?");
+var car2 = prompt("How long does it take your second car to go 100 yard in seconds?");
 
-var dryTime = prompt("How long do you put your laundry in the dryer for?");
-
-var hours = prompt("How many hours do you have before you leave?");
-
-var minutesLeft = hours * 60;
-
-console.log(minutesLeft);
-//var totalWashTime = washTime + dryTime;
-
-//var totalLaundryTime = totalWashTime * loads;
-
-/*(minutesLeft > totalLaundryTime) ? console.log("Yes, you do have time to complete your laundry.") : console.log("You have too much laundry and not enough time.");*/
+(car1 === car2) ? console.log("Your cars are equal speed") : console.log("One car is faster than the other?");
