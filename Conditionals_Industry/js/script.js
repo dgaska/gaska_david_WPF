bd16283bf2aeae0d7f0e6387f9183f251544f513
David Gaska <dgaska@fullsail.edu>
2013-10-15 20:26:02 -0500

    added comments

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -11,28 +11,31 @@
 
 
 
-const packagesPerCase = 10;
+const packagesPerCase = 10; // Number of packages of chicken per case
 
 
-var comingIn = prompt("How many cases of chicken are coming in today?");
+var comingIn = prompt("How many cases of chicken are coming in today?"); // asking the user how many cases are coming in today
 
-var inCooler = prompt("How many cases of chicken are left in the the coooler?");
+var inCooler = prompt("How many cases of chicken are left in the the coooler?"); // asking the user how many cases are in the cooler already
 
-var coolerPackages = inCooler * packagesPerCase;
+var coolerPackages = inCooler * packagesPerCase; // equation to figure out how many packages are in the cooler
 
-var delivaryTotal = comingIn * packagesPerCase;
+var delivaryTotal = comingIn * packagesPerCase; // equation to figure out how many packages are being delivered
 
 //console.log(coolerPackages);
 
-var totalPackages = delivaryTotal + coolerPackages;
+var totalPackages = delivaryTotal + coolerPackages; // the total number of packages that you currently have on hand
 
 //console.log(totalPackages);
 
 if(totalPackages <= 1000){
+	// printed if true meaning you need to order more
 	console.log("You need to order more chicken leg quarters for the weekend!");
 }else if(totalPackages >= 1500){
+	// printed if there too many 
 	console.log("You have too many packages already, must reduce the price for the weekend!");
 }else{
+	// printed if you there is no need to do anything
 	console.log("You have a good amount, no need to order more.");
 }
 
