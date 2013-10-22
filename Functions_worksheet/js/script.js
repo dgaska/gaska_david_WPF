45a997eda971c6aa89b54e230dff5b52a4745a3e
David Gaska <dgaska@fullsail.edu>
2013-10-21 19:43:57 -0500

    circumference

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -1,2 +1,10 @@
 // David Gaska 10-2013 functions worksheet
 
+// Circumference
+var total = calcCircumference(4)
+
+function calcCircumference(radius){
+	var circumference = 2 * 3.14 * radius;
+	return circumference;
+}
+console.log("The circumference of the circle is " + total);
