ee6b7fb2e6e1cd2ff23e38bd820519e637e29d17
David Gaska <dgaska@fullsail.edu>
2013-10-21 20:00:55 -0500

    Stings

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -1,10 +1,19 @@
 // David Gaska 10-2013 functions worksheet
 
 // Circumference
-var total = calcCircumference(4)
+var total = calcCircumference(4);
 
 function calcCircumference(radius){
 	var circumference = 2 * 3.14 * radius;
 	return circumference;
 }
 console.log("The circumference of the circle is " + total);
+
+// Stung!
+var totalStings = beeStings(180)
+
+function beeStings(lbs){
+	var stings = 8.666666667 * lbs;
+	return stings;
+}
+console.log("It takes " + totalStings + " to kill this animal.");
