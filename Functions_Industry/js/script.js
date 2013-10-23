7024b19a352cb6f082629b712d2fc2e8eb75cc80
David Gaska <dgaska@fullsail.edu>
2013-10-23 15:59:16 -0500

    finished industry function

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,16 +1,16 @@
 // David Gaska 10-2013 Functions Assingment Industry
 
 //When is my lunch
-// lunch is either at noon or after
+// lunch is either at noon or 1pm or later depending on start time
 
 function lunchHour(hours){//my function
 
-if(hours = null){ //if nothing is entered
-	console.log("Please enter in a number.");
-}else if(hours <= 7){ // if you start earlier than 7am or at 7
-	console.log("Your lunch is at noon.");
-}else{ // if you start later than 7am
-	console.log("Your lunch is after the person gets who went on lunch at noon.");
+if(hours <= 7){ //if nothing is entered
+	console.log("You lunch is at noon");
+}else if(hours > 7 && hours <= 9){ // if you start later than 7am or but before 10am
+	console.log("Your lunch is at one.");
+}else{ // if you start later than 9am
+	console.log("Your lunch is after the person gets back who went on lunch at one.");
 }
 }
 	
