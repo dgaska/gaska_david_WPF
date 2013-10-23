f5ce6ec6a630f450724321468e0347ac804d0217
David Gaska <dgaska@fullsail.edu>
2013-10-23 09:41:52 -0500

    Set up problem and variables with prompts

---
+++ Functions_Wacky/js/script.js
@@ -0,0 +1,12 @@
+// David Gaska 10-2013 Functions Assignment Wacky
+
+// How many loafs of bread do you use in a month?
+// Do you make toast in the morning?
+// If you do make toast how many pieces do you have?
+// Do you make sandwiches for lunch or dinner?
+// How many sandwiches per day do you make?
+
+var toast = prompt("I am trying to figure how many loafs of bread you use in a month? \nHow many pieces of toast do you make in a week?");
+var sandwich = prompt("How many sandwiches do you make in week?");
+
+
