35c4425ec217e8f1b0da635ab6ceab40f9f8cc07
David Gaska <dgaska@fullsail.edu>
2013-10-23 09:45:21 -0500

    set up some constants

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -7,6 +7,10 @@
 // How many sandwiches per day do you make?
 
 var toast = prompt("I am trying to figure how many loafs of bread you use in a month? \nHow many pieces of toast do you make in a week?");
-var sandwich = prompt("How many sandwiches do you make in week?");
+// prompt asking for slices of toast made per week
+var sandwich = prompt("How many sandwiches do you make in week?"); //prompt asking how many sandwiches they make in a week
 
+const slicesPerSandwich = 2 // Avg slices used per sandwich
+const slicesPerLoaf = 26 // Avg slices of bread per loaf
 
+function loafs
