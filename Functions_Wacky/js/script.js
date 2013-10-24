eaa71cb7a2594190a1803c852e3cbad5cdf06d9d
David Gaska <dgaska@fullsail.edu>
2013-10-24 11:13:24 -0500

    added comments to each line of code

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -6,11 +6,10 @@
 // Do you make sandwiches for lunch or dinner?
 // How many sandwiches per day do you make?
 
-var totalLoafs = function(toast, sandwich){
-	var loafs = (toast * 1 + sandwich * 1) / 26;
-	return loafs;
+var totalLoafs = function(toast, sandwich){ // Anoymous Function
+	var loafs = (toast * 1 + sandwich * 1) / 26; // var with formula for answer
+	return loafs; // return function
 }
-var total = totalLoafs(prompt("I am collecting data to find out how much bread a person uses in a week. \nHow many pieces of toast do you make in a week?"),prompt("How many sandwiches do u make in a week?") * 2); // prompt to collect how many pieces of toast
- // prompt to collect number of sandwiches are made and then multiply by 2 since you normally use 2 slices of bread.
+var total = totalLoafs(prompt("I am collecting data to find out how much bread a person uses in a week. \nHow many pieces of toast do you make in a week?"),prompt("How many sandwiches do u make in a week?") * 2); // prompt to collect how many pieces of toast & prompt to collect number of sandwiches are made and then multiply by 2 since you normally use 2 slices of bread.
 
-console.log(total);
+console.log("You use " + total + " loafs of bread per week!"); // Printed result to the console.
